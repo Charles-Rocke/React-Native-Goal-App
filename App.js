3e59e2af167ac968825a2d4afbb80db3e39ed169
SpOOk <spook.devv@gmail.com>
2023-08-16 21:07:14 -0400

    updated packages

---
+++ App.js
@@ -0,0 +1,36 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your course goal"/>
+        <Button title="Add Goal" />
+      </View>
+      <View>
+        <Text>List of Goals</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+  // Max styles
+  appContainer: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    borderRadius: 5,
+    width: "80%",
+    marginRight: 8,
+    padding: 8,
+  },
+});
